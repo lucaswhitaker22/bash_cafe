2b985361c7334bb4395c41d29037cf2d228cc245
Lucas Whitaker <lucaswhitaker22@gmail.com>
2019-08-06 10:18:35 -0400

    Update bash_cafe.sh

--- bash_cafe.sh
+++ bash_cafe.sh
@@ -6,27 +6,6 @@ expense=1
 sales_mult=1
 cash=100
 mult=0
-start_time=$(date +"%s")
-
-center () {
-    width=$(tput cols)
-    height=$(tput lines)
-    str="$1"
-    length=${#str}
-    tput cup $((height / 2)) $(((width / 2) - (length / 2)))
-    if [[ $str == *"Day"* ]];then
-        tput rev;echo "$str";  tput sgr0
-    else
-        echo "$str"
-    fi
-#!/bin/bash
-#!/usr/bin/env bash
-
-day_num=6
-expense=1
-sales_mult=1
-cash=100
-mult=0
 
 center () {
     width=$(tput cols)
@@ -165,74 +144,3 @@ clear
     clear
     save
 done
-
-    if (( sales > count )); then
-        income=$(( count*cost ))
-    else
-        income=$(( sales*cost ))
-    fi
-    echo income: $income
-    #calculate profit
-    profit=$(( income-count-$((sales/2)) ))
-    return $profit
-}
-
-read -p "Would you like to load your saved game? (y/n) >>> " input
-if [[ $input == "y" ]]; then
-    load
-fi
-clear
-
-name="Coffee Shop"
-#start_shop
-while true
-do
-clear
-    profit=0
-    #tput smul; echo -e " Day $day_num "; tput sgr0
-    center " Day $day_num "
-    sleep 1
-    tput clear
-    weather=$(( 20+$RANDOM%15 ))
-    center "Weather: $weather"
-    sleep 1
-    tput clear
-    tput rev;echo " Cash: $cash ";tput sgr0
-    echo ""
-    while true; do
-        read -p "How many coffees do you wish make? >>> " coffee_cnt
-        if (( $coffee_cnt*$expense > $cash)) || [ ! -z "${coffee_cnt##[0-9]*}" ]; then
-            echo "You cannot do that!"
-        else
-            break
-        fi
-    done
-    while true; do
-        read -p "How much do you wish to charge per coffee? >>> " coffee_cost
-        if [ ! -z "${coffee_cost##[0-9]*}" ]; then
-            echo "You cannot do that!"
-        else
-            break  
-        fi
-    done
-    read -p "Press enter to open shop: "
-    clear
-    open_shop
-    echo -e "\e[1;7m Day $day_num: Summary \e[0m"    
-    calculate_profit $weather $coffee_cost $coffee_cnt
-    echo "Profit: $profit"
-    cash=$(( $cash+$profit ))
-    
-        if ! (( $day_num % 7 )) ; then
-            cash=$(( cash-50 ))
-            echo ""
-            echo -e "\e[31mYou paid $"50" in weekly bills\e[0m"
-        fi
-    
-    day_num=$(( day_num+1 ))
-    echo ""
-    read -p "Press enter to continue: "
-    clear
-    save
-done
-
