43ad60406d344e8d6cd79f0b9cd1af147f6faba0
Lucas Whitaker <lucaswhitaker22@gmail.com>
2019-08-08 14:08:45 -0400

    Add files via upload

--- scripts/functions/save_load_functions.sh
+++ scripts/functions/save_load_functions.sh
@@ -1,16 +1,15 @@
 #!/bin/bash
 #!/usr/bin/env bash
 
-shop_name=""
 new_save () {
-    while true; do
-        read -p "What would you like to name your shop? >>> " shop_name
-        if [[ $shop_name =~ [^a-zA-Z0-9_-] ]]; then
-            echo "Shop name contains invalid characters, please try again!"
-        else
-            break
-        fi
-    done
+while true; do
+    read -p "What would you like to name your shop? >>> " shop_name
+    if [[ $shop_name =~ [^a-zA-Z0-9_-] ]]; then
+        echo "Shop name contains invalid characters, please try again!"
+    else
+        break
+    fi
+done
 }
 
 #outputs the list of saves in folder
@@ -19,7 +18,11 @@ get_saves () {
     if [[ -z "$saves" ]]; then
         return 1
     fi    
-    return 0 
+    echo -e "\e[4m Your shops: \e[0m"
+    for i in "${saves[@]}"; do
+        echo " - $i"
+    done
+    return 0
 }
 
 #creates new save file and writes to it
@@ -34,17 +37,18 @@ save () {
 #asks user which load to open and reads it
 load () {
     #get list of save files
-    echo -e "\e[4m Your shops: \e[0m"
-    for i in "${saves[@]}"; do
-        echo " - $i"
-    done
+    get_saves
+    if [[ $? == 1 ]]; then
+        echo "There are no saves to load"
+        new_save
+        return 0
+    fi
     #check if save file exists
     while true; do
         read -p "Which shop would you like to load? >>> " input
         save_file=$input"_save.txt"
         if [[ $(ls) == *"$save_file"* ]]; then
             #load saves file, parse its content into global variables
-            shop_name=$input
             saved_data=$(cat $save_file)
             day_num=$(grep "day_num" <<< "$saved_data"|awk -F "=" '{print $2}')
             cash=$(grep "cash" <<< "$saved_data"|awk -F "=" '{print $2}')
